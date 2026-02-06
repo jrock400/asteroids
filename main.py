f8d8992430c8e8de0e1a4430a96470bcf51848b0
jrock400 <git.luxurious804@passmail.net>
2026-02-06 16:41:49 -0500

    f: circlshape

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from logger import log_state
 
 
 def main():
-    print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    #print(f"Starting Asteroids with pygame version:{pygame.version.ver}")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -18,6 +22,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = (clock.tick(60)) / 1000
 
 if __name__ == "__main__":
     main()
