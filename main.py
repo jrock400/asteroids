c804d56714d493375c9293ea7f32c80993a4f2bb
jrock400 <git.luxurious804@passmail.net>
2026-02-06 17:14:59 -0500

    g: sprites and triangles

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH,SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -11,14 +12,16 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT /2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
