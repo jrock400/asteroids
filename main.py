99909f699158e1930db08ee0ea0f579b2eff2ac6
jrock400 <git.luxurious804@passmail.net>
2026-02-07 14:13:40 -0500

    j: groups

--- main.py
+++ main.py
@@ -10,23 +10,32 @@ def main():
     #print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     player = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT /2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+
+    #Game Loop
+
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+        
+        if event.type == pygame.QUIT:
+            return
         dt = (clock.tick(60)) / 1000
 
 if __name__ == "__main__":
