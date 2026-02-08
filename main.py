5ff9b6ccd8a7dbfc2b40d6667c0dd88d5a6a57f5
jrock400 <git.luxurious804@passmail.net>
2026-02-08 08:01:41 -0500

    l: collision

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state,log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import *
+import sys
 
 
 def main():
@@ -35,6 +36,11 @@ def main():
         
         screen.fill("black")
         updatable.update(dt)
+        for thing in asteroids:
+            if thing.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for thing in drawable:
             thing.draw(screen)
         pygame.display.flip()
